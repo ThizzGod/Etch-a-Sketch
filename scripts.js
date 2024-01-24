571801d6c6524ded48e86adf8484e2ae852ff601
Max Barker <maxbarker26@gmail.com>
2024-01-24 09:57:39 -0900

    Require input to be less than 100
    The if statement to break the div creating for loop must go at the top of the loop
    so that it will break the loop before the first div is created

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ let page = document.querySelector("#page");
 let blockNumber = function () {
     let number = prompt("How many blocks per row?");
     for (i = 0; i < number**2; i++) {
+        if (number > 100) {
+            alert("Please choose a number less than 100");
+            break
+        }
         let i = document.createElement("div");
         page.appendChild(i);
         let x = (100 / (+number)) + "%"
