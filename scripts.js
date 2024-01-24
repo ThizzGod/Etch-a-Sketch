c4b2269509a3125e61d0ba9de3c295d7e04a6905
Max Barker <maxbarker26@gmail.com>
2024-01-23 15:35:51 -0900

    Add event listener inside div creating loop
    Event listener toggles css class to change background to yellow on

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
 
 let page = document.querySelector("#page");
 
-for (i = 0; i < 16; i++) {
+
+for (i = 0; i < 9**2; i++) {
     let i = document.createElement("div");
     page.appendChild(i);
+    //i.classList.add(`bg-color`);
+    i.addEventListener("mouseenter", () => {
+        i.classList.add("bg-color")
+    })
+
 }
 
+
+
+//use blocknumber to find min-width percent to fit that many blocks in one row
+//formula is (blockNumber + 1)/100 round down to two decimal places
 
 
